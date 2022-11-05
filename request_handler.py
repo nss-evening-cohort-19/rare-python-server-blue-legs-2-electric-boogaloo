f07ac5740f6aabd8974a79ce0a2d85cd79948128
Justin Ferwerda <justin.ferwerda@gmail.com>
2022-11-05 11:46:39 -0500

    added get tags by post id to request handler

--- request_handler.py
+++ request_handler.py
@@ -48,7 +48,8 @@ from views import (
                    get_posts_by_category,
                    get_post_reactions_by_post_id,
                    get_single_user,
-                   get_posts_by_author_id
+                   get_posts_by_author_id,
+                   get_post_tags_by_post_id
 )
 
 class HandleRequests(BaseHTTPRequestHandler):
@@ -164,6 +165,9 @@ class HandleRequests(BaseHTTPRequestHandler):
                 
             elif key == "author_id" and resource == "posts":
                 response = get_posts_by_author_id(value)
+            
+            elif key == "post_id" and resource == "tags":
+                response = get_post_tags_by_post_id(value)
  
         self.wfile.write(response.encode())
         
